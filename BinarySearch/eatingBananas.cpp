12f3efde3e2a8d28f1940ea8ad8bc3f9471387ad
Deven Agarwal <dvn.agarwal24@gmail.com>
2023-07-06 09:15:26 +0530

    Koko eating bananas

---
+++ BinarySearch/eatingBananas.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool isPossible(vector<int> v, int h, long long int n) {
+
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] <= n) {
+            h--;
+        } else {
+            long long tmp;
+            if (v[i] % n == 0) {
+                tmp = (v[i] / n);
+            } else {
+                tmp = (v[i] / n) + 1;
+            }
+
+            h -= tmp;
+        }
+
+        if (h < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int minimumRateToEatBananas(vector<int> v, int h) {
+    int low = 1;
+    // Here we don't need to take the range till the sum of all elements
+    // Koko can only eat upto the maximum number in the pile at once. If it tries to eat any number of bananas
+    // more than the maximum, then the amount of time taken will be the constant
+    // Here we are trying to find the mininum number of bananas it can eat in an hour, and so we take the maximum of
+    // array as the highest value
+    int high = *max_element(v.begin(), v.end());
+
+    int ans = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low)/2;
+
+        if (isPossible(v, h, mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {7, 15, 6, 3};
+
+    cout << minimumRateToEatBananas(v, 8) << endl;
+
+    v.clear();
+    v = {4, 9, 4, 3, 1, 3};
+
+    cout << minimumRateToEatBananas(v, 66) << endl;
+
+    return 0;
+}
