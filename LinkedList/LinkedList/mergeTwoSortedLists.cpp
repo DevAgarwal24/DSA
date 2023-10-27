029f043df2827657fb60083f7c08705dcecb6b39
Deven Agarwal <dvn.agarwal24@gmail.com>
2023-10-27 07:47:45 +0530

    Add another way of writing the solution

--- LinkedList/LinkedList/mergeTwoSortedLists.cpp
+++ LinkedList/LinkedList/mergeTwoSortedLists.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // T.C = O(n+m)
 // S.C = O(1)
+#if 0
 Node *mergeTwoSortedLists(Node *l1, Node *l2) {
     Node *tmp = new Node();
     Node *curr = tmp;
@@ -32,7 +33,35 @@ Node *mergeTwoSortedLists(Node *l1, Node *l2) {
 
     return tmp->next;
 }
+#endif
 
+// Another code for T.C = (n+m)
+// In this solution, the first node of list1 will always be having the value less
+// than the first node of list2
+Node *mergeTwoSortedLists(Node *l1, Node *l2) {
+    if (l1 == nullptr) return l2;
+    if (l2 == nullptr) return l1;
+
+    if (l1->getData() > l2->getData()) swap(l1, l2);
+
+    Node *res = l1;
+
+    while (l1 && l2) {
+        // This will point to the node previous to the node pointed by l1
+        Node *tmp = nullptr;
+        while (l1 && l1->getData() <= l2->getData()) {
+            tmp = l1;
+            l1 = l1->next;
+        }
+
+        tmp->next = l2;
+        
+        // swap l1 and l2 again so that l1 points to the node with smaller value
+        swap(l1, l2);
+    }
+
+    return res;
+}
 
 int main()
 {
