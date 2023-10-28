2322b74b947baa8e1847f3fd1ec10042b94d2424
Deven Agarwal <dvn.agarwal24@gmail.com>
2023-10-28 11:33:20 +0530

    Add code where new nodes are created instead of modifying the existing ones

--- LinkedList/LinkedList/addTwoNumbers.cpp
+++ LinkedList/LinkedList/addTwoNumbers.cpp
@@ -1,5 +1,5 @@
-// 
-//
+// https://leetcode.com/problems/add-two-numbers/description/
+// https://www.codingninjas.com/studio/problems/add-two-numbers_1170520
 
 #include <iostream>
 #include <vector>
@@ -8,8 +8,11 @@
 
 using namespace std;
 
+// There is just the optimal solution here
+// Don't just go for the solution directly, ask about edge cases and not jump to the solution directly
 
 // Modifying the exisiting list
+#if 0
 Node *addTwoNumbers(Node *num1, Node *num2)
 {
     if (!num1) return num2;
@@ -58,7 +61,51 @@ Node *addTwoNumbers(Node *num1, Node *num2)
 
     return res;
 }
+#endif
 
+// Creating a new list
+Node *addTwoNumbers(Node *num1, Node *num2)
+{
+    Node *res = new Node();
+    Node *curr = res;
+
+    int carry = 0;
+
+    while (num1 && num2) {
+        int sum = num1->data + num2->data + carry;
+
+        carry = sum / 10;
+        Node *tmp = new Node(sum % 10);
+
+        curr->next = tmp;
+        curr = tmp;
+
+        num1 = num1->next;
+        num2 = num2->next;
+    }
+
+    if (num2) {
+        swap(num1, num2); 
+    }
+
+    while (num1) {
+        int sum = num1->data + carry;
+
+        carry = sum / 10;
+        Node *tmp = new Node(sum % 10);
+
+        curr->next = tmp;
+        curr = tmp;
+
+        num1 = num1->next;
+    }
+
+    if (carry) {
+        curr->next = new Node(1);
+    }
+
+    return res->next;
+}
 
 int main()
 {
@@ -68,11 +115,16 @@ int main()
     Node *num1 = constructLL(v1);
     Node *num2 = constructLL(v2);
 
-    num2 = addTwoNumbers(num1, num2);
-    printLinkedList(num2);      // 4 4 9 4 5
+    Node *result = nullptr;
+
+    result = addTwoNumbers(num1, num2);
+    printLinkedList(result);      // 4 4 9 4 5
 
     v1.clear();
     v2.clear();
+
+    num1 = deleteLinkedList(num1);
+    num2 = deleteLinkedList(num2);
 
     v1 = {9, 9, 9, 9};
     v2 = v1;
@@ -80,8 +132,11 @@ int main()
     num1 = constructLL(v1);
     num2 = constructLL(v2);
 
-    num2 = addTwoNumbers(num1, num2);
-    printLinkedList(num2);      // 8 9 9 9 1
+    result = addTwoNumbers(num1, num2);
+    printLinkedList(result);      // 8 9 9 9 1
+
+    num1 = deleteLinkedList(num1);
+    num2 = deleteLinkedList(num2);
 
     return 0;
 }
