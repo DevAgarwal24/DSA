b761a4abe91854c27deec49fe4b7d1080e70edaa
Deven Agarwal <dvn.agarwal24@gmail.com>
2024-01-01 10:21:58 +0530

    Logic for trying to space optimise

--- DynamicProgramming/frogJumpK.cpp
+++ DynamicProgramming/frogJumpK.cpp
@@ -52,6 +52,15 @@ int frogJump(int n, int k, vector<int> &height){
     return arr[n];
 }
 
+// Space Optimization from O(N) to O(K)
+// We can do that by eliminating the arr vector and create another vector of size k
+// After each iteration, we have to left shift the values of the new vector by 1 and
+// add the current value at the last
+// Maybe we can use a list and delete the last node and then add the current value to the
+// starting node. Only problem, the search time to find i-jth value will increase
+// But in worst case if K = N, there is no space optimization and it will rather take
+// more time
+
 int main()
 {
     vector<int> height = {1, 4, 8, 7, 5};
