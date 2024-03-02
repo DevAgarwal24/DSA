6ad0b7c39b6410f93af87c2d3261672acce0b61b
Deven Agarwal <dvn.agarwal24@gmail.com>
2024-03-02 13:41:09 +0530

    Fix indentation

--- BinarySearch/kthMissingPositiveNumber.cpp
+++ BinarySearch/kthMissingPositiveNumber.cpp
@@ -33,15 +33,15 @@ int missingK(vector<int> &vec, int n, int k) {
 }
 
 int missingK2(vector<int> &vec, int n, int k) {
-	for (int i = 0; i < n; i++) {
-		if (vec[i] <= k) {
-			k++;
-		} else {
-			break;
-		}
-	}
+    for (int i = 0; i < n; i++) {
+	    if (vec[i] <= k) {
+		    k++;
+	    } else {
+            break;
+	    }
+    }
 
-	return k;
+    return k;
 }
 
 int missingK3(vector<int> &vec, int n, int k) {
@@ -51,26 +51,25 @@ int missingK3(vector<int> &vec, int n, int k) {
     while (low <= high) {
         int mid = low + (high - low)/2;
 
-		// This tells how many numbers are missing
+	    // This tells how many numbers are missing
         int diff = vec[mid] - (mid + 1);
 
-		// If the numbers missing are less than the kth missing to be found
+        // If the numbers missing are less than the kth missing to be found
         // it will mean that the number will be on the left side
         if (diff < k) {
             low = mid + 1;
-                
         } else {
             high = mid - 1;
         }
     }
 
-	// low will point to the index where the missing numbers are greater than k and
-	// high will point to the index where the missing numbers are less than k
-	// answer will be vec[high] + extra numbers required
-	// But there can be a case where high can point to -1
-	// extra number required = (k - missing) where missing = vec[high] - (high + 1)
-	// Simplyfying,
-	// vec[high] + (k - (vec[high] - (high + 1)))
+    // low will point to the index where the missing numbers are greater than k and
+    // high will point to the index where the missing numbers are less than k
+    // answer will be vec[high] + extra numbers required
+    // But there can be a case where high can point to -1
+    // extra number required = (k - missing) where missing = vec[high] - (high + 1)
+    // Simplyfying,
+    // vec[high] + (k - (vec[high] - (high + 1)))
 
     return high + 1 + k;	// this is same as low + k
 }
@@ -79,14 +78,14 @@ int main()
 {
 	vector<int> v = {4, 7, 9, 10};
 
-	cout << missingK(v, v.size(), 1) << endl;	// 1
-	cout << missingK2(v, v.size(), 1) << endl;
-	cout << missingK3(v, v.size(), 1) << endl;
+    cout << missingK(v, v.size(), 1) << endl;   // 1
+    cout << missingK2(v, v.size(), 1) << endl;
+    cout << missingK3(v, v.size(), 1) << endl;
 
-	cout << endl;
+    cout << endl;
     cout << missingK(v, v.size(), 4) << endl;	// 5
     cout << missingK2(v, v.size(), 4) << endl;	// 5
-	cout << missingK3(v, v.size(), 4) << endl;
+    cout << missingK3(v, v.size(), 4) << endl;
 
 	return 0;
 }
